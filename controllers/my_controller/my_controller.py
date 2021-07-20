91bc33703a833c0e73ce89febfed528f9b732d40
jeffcheng1230 <jeffreycheng1230@gmail.com>
2021-07-20 09:04:08 -0400

    updated day5.pdf

--- controllers/my_controller/my_controller.py
+++ controllers/my_controller/my_controller.py
@@ -1,79 +1,4 @@
-rotations = None
-leftSpeed = None
-rightSpeed = None
-left_wheel = None
-right_wheel = None
-
-# Describe this function...
-def stop():
-  global rotations, leftSpeed, rightSpeed, left_wheel, right_wheel
-  getEncoders(encObj[left_wheel])
-  lastEncReset[encObj[left_wheel]] = encCount[encObj[left_wheel]]
-  getEncoders(encObj[right_wheel])
-  lastEncReset[encObj[right_wheel]] = encCount[encObj[right_wheel]]
-  set_speed_for_both_motors(0, 0)
-
-# Describe this function...
-def setup_motor_objs():
-  global rotations, leftSpeed, rightSpeed, left_wheel, right_wheel
-  left_wheel = myRobot.getDevice("left wheel")
-  encObj[left_wheel] = left_wheel.getPositionSensor()
-  left_wheel.setPosition(float("inf"))
-  left_wheel.setVelocity(0)
-  encObj[left_wheel].enable(timeStep)
-  encCount[left_wheel] = 0
-  lastEncReset[encObj[left_wheel]] = 0
-
-  right_wheel = myRobot.getDevice("right wheel")
-  encObj[right_wheel] = right_wheel.getPositionSensor()
-  right_wheel.setPosition(float("inf"))
-  right_wheel.setVelocity(0)
-  encObj[right_wheel].enable(timeStep)
-  encCount[right_wheel] = 0
-  lastEncReset[encObj[right_wheel]] = 0
-
-
-# Describe this function...
-def turn_right():
-  global rotations, leftSpeed, rightSpeed, left_wheel, right_wheel
-  getEncoders(encObj[left_wheel])
-  lastEncReset[encObj[left_wheel]] = encCount[encObj[left_wheel]]
-  getEncoders(encObj[right_wheel])
-  lastEncReset[encObj[right_wheel]] = encCount[encObj[right_wheel]]
-  while myRobot.step(timeStep) != -1 and (getEncoders(encObj[left_wheel]) or encCount[encObj[left_wheel]] - lastEncReset[encObj[left_wheel]]) <= 160:
-    if gyroEnable:
-      updateGyro()
-    set_speed_for_both_motors(50, -48)
-
-# Describe this function...
-def turn_left():
-  global rotations, leftSpeed, rightSpeed, left_wheel, right_wheel
-  getEncoders(encObj[left_wheel])
-  lastEncReset[encObj[left_wheel]] = encCount[encObj[left_wheel]]
-  getEncoders(encObj[right_wheel])
-  lastEncReset[encObj[right_wheel]] = encCount[encObj[right_wheel]]
-  while myRobot.step(timeStep) != -1 and (getEncoders(encObj[right_wheel]) or encCount[encObj[right_wheel]] - lastEncReset[encObj[right_wheel]]) <= 160:
-    if gyroEnable:
-      updateGyro()
-    set_speed_for_both_motors(-48, 50)
-
-# Describe this function...
-def move_forward_for___of_rotations(rotations):
-  global leftSpeed, rightSpeed, left_wheel, right_wheel
-  getEncoders(encObj[left_wheel])
-  lastEncReset[encObj[left_wheel]] = encCount[encObj[left_wheel]]
-  getEncoders(encObj[right_wheel])
-  lastEncReset[encObj[right_wheel]] = encCount[encObj[right_wheel]]
-  while myRobot.step(timeStep) != -1 and (getEncoders(encObj[left_wheel]) or encCount[encObj[left_wheel]] - lastEncReset[encObj[left_wheel]]) <= rotations * 360:
-    if gyroEnable:
-      updateGyro()
-    set_speed_for_both_motors(50, 50)
-
-# Describe this function...
-def set_speed_for_both_motors(leftSpeed, rightSpeed):
-  global rotations, left_wheel, right_wheel
-  left_wheel.setVelocity((leftSpeed / 100.0) * left_wheel.getMaxVelocity())
-  right_wheel.setVelocity((rightSpeed / 100.0) * right_wheel.getMaxVelocity())
+dist = None
 
 
 from controller import Robot
@@ -141,25 +66,9 @@ encCount = {}
 lastEncReset = {}
 myRobot.step(timeStep)
 
-left_wheel = myRobot.getDevice("left wheel")
-encObj[left_wheel] = left_wheel.getPositionSensor()
-left_wheel.setPosition(float("inf"))
-left_wheel.setVelocity(0)
-encObj[left_wheel].enable(timeStep)
-encCount[left_wheel] = 0
-lastEncReset[encObj[left_wheel]] = 0
-
-right_wheel = myRobot.getDevice("right wheel")
-encObj[right_wheel] = right_wheel.getPositionSensor()
-right_wheel.setPosition(float("inf"))
-right_wheel.setVelocity(0)
-encObj[right_wheel].enable(timeStep)
-encCount[right_wheel] = 0
-lastEncReset[encObj[right_wheel]] = 0
-
-print(getEncoders(encObj[left_wheel]) or encCount[encObj[left_wheel]] - lastEncReset[encObj[left_wheel]])
-stop()
-for count in range(4):
-  move_forward_for___of_rotations(3.55)
-  turn_right()
-  stop()
+dist = myRobot.getDevice('distanceSensor')
+dist.enable(timeStep)
+while myRobot.step(timeStep) != -1 and True:
+  if gyroEnable:
+    updateGyro()
+  print(dist.getValue())
